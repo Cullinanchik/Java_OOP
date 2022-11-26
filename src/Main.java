f6ff8ec7ebdf35c42e0788ed396325f68bb5d5c4
ilapavlov <mr.ilya.pavlov@gmail.com>
2022-11-26 21:24:21 +0300

    Первая задача решена

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Human human_1 = new Human();
+        Human human_2 = new Human();
+        Human human_3 = new Human();
+
+        human_1.age = 20;
+        human_1.name = "Ilya";
+        human_1.weight = 65;
+
+        human_2.age = 20;
+        human_2.name = "Vladislav";
+        human_2.weight = 110;
+
+        human_3.age = 30;
+        human_3.name = "Vladislav";
+        human_3.weight = 64;
+
+        int average = (human_1.age + human_2.age + human_3.age) / 3;
+        System.out.println(average);
+    }
+}
